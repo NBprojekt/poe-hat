155ed8d750a8c3f1e6d6b0fe466faeda7eebe711
Norbert Bartko <bartko.norbert@gmail.com>
2021-06-24 15:18:55 +0200

    move poe hat file into lib/nbprojekt

--- lib/nbprojekt/poe-hat.py
+++ lib/nbprojekt/poe-hat.py
@@ -10,10 +10,6 @@ import socket
 import fcntl
 import struct
 
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
 from PIL import Image,ImageDraw,ImageFont
 from waveshare import SSD1306
 
@@ -29,39 +25,39 @@ class PoeHat:
         self.address = address
         self.maxTemp = maxTemp
         screen.ClearBlack()
-        
+
     def fanOn(self):
         self.i2c.write_byte(self.address, 0xFE & self.i2c.read_byte(self.address))
-        
+
     def fanOff(self):
         self.i2c.write_byte(self.address, 0x01 | self.i2c.read_byte(self.address))
-        
+
     def getIp(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 80))
         ip = s.getsockname()[0]
         s.close()
         return ip
-        
+
     def getTemp(self):
         with open('/sys/class/thermal/thermal_zone0/temp', 'rt') as f:
             temp = (int)(f.read() ) / 1000.0
         return ((int)(temp*10))/10.0
-    
+
     def updateDisplay(self):
         image = Image.new('1', (screen.width, screen.height), 'WHITE')
-        draw = ImageDraw.Draw(image)  
+        draw = ImageDraw.Draw(image)
         ip = self.getIp()
         temp = self.getTemp()
 
         draw.text((0,1), 'IP: ' + str(ip), font = font, fill = 0)
         draw.text((0,15), 'Temp: ' + str(temp), font = font, fill = 0)
-        
+
         if(temp >= self.maxTemp):
             draw.text((77,16), 'FAN:ON', font = font, fill = 0)
             self.FAN_ON()
         else:
             draw.text((77,16), 'FAN:OFF', font = font, fill = 0)
             self.FAN_OFF()
-            
+
         screen.ShowImage(screen.getbuffer(image))
